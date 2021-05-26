a8e0c60d7d9f70e025577939aa8be7acb0cafe14
tlalnunhlima <32595465+tlalnunhlima@users.noreply.github.com>
2021-05-26 17:33:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(6000, () => {
-    console.log('App is listening on port 6000')
+app.listen(3000, () => {
+    console.log('App is listening on port 3000')
 });
