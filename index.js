1f0da06a413f0024e48abb14c8279f4a269f806c
tlalnunhlima <tlalnunhlima@gmail.com>
2021-05-27 14:31:14 +0530

    commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require('express')
-
 const ejs = require('ejs')
 
+
 const app = express();
-
+//view engine
 app.set('view engine', 'ejs');
-
-// public
+// public express static
 app.use(express.static(__dirname + '/public'));
 
+
+//home page
 app.get('/', (req, res) => {
     res.render('signin')
 });
 
+app.get('/users/register', (req, res) => {
+    res.render('register')
+});
 
+
+//listen on specific post
 app.listen(3000, () => {
     console.log('App is listening on port 3000')
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> a8e0c60d7d9f70e025577939aa8be7acb0cafe14
